8490349e5f2c569748001b1d59661e901da98463
David Parker <dlparker1005@gmail.com>
2023-11-30 13:49:08 -0500

    Adding email template variable

--- includes/emails.php
+++ includes/emails.php
@@ -21,3 +21,90 @@ function pmprogroupacct_email_templates( $templates ) {
 	return $templates;
 }
 add_filter( 'pmproet_templates', 'pmprogroupacct_email_templates' );
+
+/**
+ * Add an email template variable to show group data when a parent purchases a level with a group.
+ *
+ * @since TBD
+ *
+ * @param array $data The email template data.
+ * @param PMProEmail $email The email object.
+ * @return array The email template data.
+ */
+function pmprogroupacct_email_data( $data, $email ) {
+	// Set the group data in case we bail early.
+	$data['pmprogroupacct_group_info'] = '';
+
+	// If 'checkout' is not in the email template name, bail.
+	if (  empty( $email->template ) || strpos( $email->template, 'checkout' ) === false ) {
+		return $data; 
+	}
+
+	// Get the login for the user who this checkout is for.
+	if ( empty( $email->data['user_login'] ) ) {
+		return $data;
+	}
+
+	// Get the user object.
+	$user = get_user_by( 'login', $email->data['user_login'] );
+	if ( empty( $user ) ) {
+		return $data;
+	}
+
+	// Get the level ID that was purchased.
+	$level_id = empty( (int)$data['membership_id'] ) ? null : (int)$data['membership_id'];
+	if ( empty( $level_id ) ) {
+		return $data;
+	}
+
+	// Check if there is a group for this user and level.
+	$group = PMProGroupAcct_Group::get_group_by_parent_user_id_and_parent_level_id( $user->ID, $level_id );
+	if ( empty( $group ) ) {
+		return $data;
+	}
+
+	// If this group doesn't have any open seats, bail.
+	if ( ! $group->is_accepting_signups() ) {
+		return $data;
+	}
+
+	// Get the group settings for the purchased level.
+	$settings = pmprogroupacct_get_settings_for_level( $level_id );
+	if ( empty( $settings ) ) {
+		return $data;
+	}
+
+	// Get the level IDs that can be purchased using this group.
+	$child_level_ids = empty( $settings['child_level_ids'] ) ? array() : $settings['child_level_ids'];
+
+	// Build the group info section.
+	ob_start();
+	?>
+	<p><?php esc_html_e( 'Your Group Code is:', 'pmpro-group-accounts' ); ?> <strong><?php echo esc_html( $group->group_checkout_code ); ?></strong></p>
+	<p><?php esc_html_e( 'New members can use this code to join your group at no additional cost.', 'pmpro-group-accounts' ); ?></p>
+	<ul>
+		<?php
+		foreach ( $child_level_ids as $child_level_id ) {
+			$child_level = pmpro_getLevel( $child_level_id );
+			$checkout_url = add_query_arg( array( 'level' => $child_level->id, 'pmprogroupacct_group_code' => $group->group_checkout_code ), pmpro_url( 'checkout' ) );
+			?>
+			<li>
+				<a href="<?php echo esc_url( $checkout_url ); ?>">
+					<?php printf( esc_html__( 'For %s membership:', 'pmpro-group-accounts' ), esc_html( $child_level->name ) ); ?>
+				</a>
+				<br />
+				<code>
+					<?php echo esc_attr( $checkout_url ); ?>
+				</code>
+			</li>
+			<?php
+		}
+		?>
+	</ul>
+	<?php
+	// Set the group data.
+	$data['pmprogroupacct_group_info'] = ob_get_clean();
+
+	return $data;
+}
+add_filter( 'pmpro_email_data', 'pmprogroupacct_email_data', 10, 2 );
